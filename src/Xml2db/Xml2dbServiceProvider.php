4dbc425004efe5de1d4af62e36683435554711ff
tibor <tibor@planetsg.com>
2016-12-02 16:34:40 +0100

    new structure

---
+++ src/Xml2db/Xml2dbServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Xml2db;
+
+use Illuminate\Support\ServiceProvider;
+
+class Xml2dbServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/Config/xml2db.php' => config_path('xml2db.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

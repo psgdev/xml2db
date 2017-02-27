44c9b0f7c788bd8f6aaf25e65e782caf5f3d7979
tibor <tibor@planetsg.com>
2017-02-27 13:58:06 +0100

    config for database where tables are created

--- src/PsgdevXml2db/Config/xml2db.php
+++ src/PsgdevXml2db/Config/xml2db.php
@@ -1,18 +1,17 @@
 <?php
 
 return [
-
     'dumpFilePath' => storage_path('logs/xml2db'),
     'parsedXmlDbNamePrefix' => 'demo_',
     'databaseConnections' =>
         [
             'xml2db' => [
                 'driver' => 'mysql',
-                'host' => env('DB_HOST', 'localhost'),
-                'port' => env('DB_PORT', '3306'),
+                'host' => env('PARSER_DB_HOST', 'localhost'),
+                'port' => env('PARSER__DB_PORT', '3306'),
                 'database' => '',
-                'username' => env('DB_USERNAME', 'demo_user'),
-                'password' => env('DB_PASSWORD', 'demo_pass'),
+                'username' => env('PARSER_DB_USERNAME', 'demo_user'),
+                'password' => env('PARSER_DB_PASSWORD', 'demo_pass'),
                 'charset' => 'utf8',
                 'collation' => 'utf8_unicode_ci',
                 'prefix' => '',

11fa3ae2aa321e53c6610b9ba0bfe6f144acd1f9
tibor <tibor@planetsg.com>
2017-02-27 14:19:46 +0100

    config correction

--- src/PsgdevXml2db/Config/xml2db.php
+++ src/PsgdevXml2db/Config/xml2db.php
@@ -8,7 +8,7 @@ return [
             'xml2db' => [
                 'driver' => 'mysql',
                 'host' => env('PARSER_DB_HOST', 'localhost'),
-                'port' => env('PARSER__DB_PORT', '3306'),
+                'port' => env('PARSER_DB_PORT', '3306'),
                 'database' => '',
                 'username' => env('PARSER_DB_USERNAME', 'demo_user'),
                 'password' => env('PARSER_DB_PASSWORD', 'demo_pass'),
